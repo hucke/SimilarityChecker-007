a67f520dd64f9d907ea7ee8554b6306760e63c92
Kim, dong su <hucke@hanmail.net>
2023-06-02 13:40:50 +0900

    리팩토링 SimilarityChecker

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -5,25 +5,19 @@ class SimilarityChecker
 public:
     int lengthPoint(std::string a, std::string b)
     {
-        if (a == "ASD" && b == "DSA")
-            return 60;
-        if (a == "A" && b == "BB")
-            return 0;
-        if ((a == "AAABB" && b == "BAA") || 
-            (a == "AA" && b == "AAA")) {
-                float point = 0.0f;
-            if (a.length() > b.length())
-            {
-                size_t gap = a.length() - b.length();
-                point = (1.0f - gap / b.length()) * 60.0f;
-            }
-            else
-            {
-                size_t gap = b.length() - a.length();
-                point = (1.0f - gap / a.length()) * 60.0f;
-            }
-            return static_cast<int>(point);
+        float point = 0.0f;
+
+        if (a.length() > b.length())
+        {
+            size_t gap = a.length() - b.length();
+            point = (1.0f - gap / b.length()) * 60.0f;
         }
-        return 0;
+        else
+        {
+            size_t gap = b.length() - a.length();
+            point = (1.0f - gap / a.length()) * 60.0f;
+        }
+
+        return static_cast<int>(point);
     }
 };

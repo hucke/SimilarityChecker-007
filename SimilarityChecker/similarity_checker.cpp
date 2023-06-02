0f64331ad88263a3f3b9e2ca2379fb59f62f75f3
Kim, dong su <hucke@hanmail.net>
2023-06-02 13:43:50 +0900

    add abs() method

--- SimilarityChecker/similarity_checker.cpp
+++ SimilarityChecker/similarity_checker.cpp
@@ -6,18 +6,29 @@ public:
     int lengthPoint(std::string a, std::string b)
     {
         float point = 0.0f;
+        size_t gap = abs(a.length(), b.length());
 
         if (a.length() > b.length())
         {
-            size_t gap = a.length() - b.length();
             point = (1.0f - gap / b.length()) * 60.0f;
         }
         else
         {
-            size_t gap = b.length() - a.length();
             point = (1.0f - gap / a.length()) * 60.0f;
         }
 
         return static_cast<int>(point);
+    }
+
+    size_t abs(size_t a, size_t b)
+    {
+        if (a > b)
+        {
+            return a - b;
+        }
+        else
+        {
+            return b - a;
+        }
     }
 };
